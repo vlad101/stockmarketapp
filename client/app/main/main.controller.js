3cdf77fc8fdc4ba8903fcfcf8aea6f173cf03333
vlad101 <V_efros85@hotmail.com>
2015-11-22 10:51:04 -0500

    completed getting graphs and instant updates on stocks being added and removed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,127 +1,153 @@
 'use strict';
 
-var testList = [];
-
 angular.module('workspaceApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
-      testList = awesomeThings;
-
       socket.syncUpdates('thing', $scope.awesomeThings);
+      
 
-      reloadChart(testList);
+      // Reload Market Chart
+      $http.get('/api/things').success(function(stockList) {
+        reloadChart(stockList);
+      });
     });
 
     // Add a stock to a list
     $scope.addThing = function() {
+
       if($scope.newThing === '') {
         return;
       }
       $http.post('/api/things', { name: $scope.newThing });
+
+      // Set form to empty
       $scope.newThing = '';
 
-      testList.push({ name: $scope.newThing });
-      
-      // ADD OBJECT REPSONSE with newsly added object
-      reloadChart(testList);
+      // Reload Market Chart
+      $http.get('/api/things').success(function(stockList) {
+        reloadChart(stockList);
+      });
     };
 
     // Remove a stock to a list
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
-      reloadChart($scope.awesomeThings);
+
+      // Reload Market Chart
+      $http.get('/api/things').success(function(stockList) {
+        reloadChart(stockList);
+      });
     };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
     });
-  });
+}); 
+
+/**
+* Reload chart
+*/
+function reloadChart(companyList) {
+
+    // Initialize chart data
+    var seriesOptions = [],
+        seriesCounter = 0,
+        startDate = '2015-01-01',
+        apiKey = 'mytMZB31ArgfkNYgsNyn';
 
 
-  /**
-  * Reload chart
-  */
+        //CHECK THIS OUT ERROR HANDLING!
 
-            // Initialize chart data
-  function reloadChart(companyList) {
-    console.log("Reloading -> " + JSON.stringify(companyList));
+        // jQuery.getJSON(
 
-    for(var company in companyList)
-      console.log(companyList[company].name);
+        //  'https://www.quandl.com/api/v3/datasets/WIKI/' +
+        //                   'MSFT1' +'.json?api_key=' + apiKey + '&' + 
+        //                   'start_date=' + startDate + '&exclude_column_names=true&order=asc', 
+        //     function(jsonResult){
+        //         console.log("Success! "  + JSON.stringify(jsonResult));
+        //     })
+        // .done(function() { console.log('getJSON request succeeded!'); })
+        // .fail(function(jqXHR, textStatus, errorThrown) { console.log('getJSON request failed! ' + textStatus); })
+        //}
 
-      var seriesOptions = [],
-          seriesCounter = 0,
-          startDate = '2015-01-01',
-          apiKey = 'mytMZB31ArgfkNYgsNyn';
-      $.each(companyList, function (i, company) {
-          $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/' +
-                     companyList[i].name +'.json?api_key=' + apiKey + '&' + 
-                     'start_date=' + startDate + '&exclude_column_names=true&order=asc' , function (jsonData) {
+    $.each(companyList, function (i, company) {
+        $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/' +
+                   companyList[i].name +'.json?api_key=' + apiKey + '&' + 
+                   'start_date=' + startDate + '&exclude_column_names=true&order=asc' , function (jsonData) {
             
-              var dataList = [ ];
-              
-              // Process jso in the following list form [[date,price],...]
-              for(var row in jsonData.dataset.data) {
-                var tempList = [ ];
-                tempList.push(new Date(jsonData.dataset.data[row][0]).getTime());
-                tempList.push(jsonData.dataset.data[row][11]);
-                dataList.push(tempList);
-              }
+            console.log(companyList[i].name + '  -->-' +jsonData.dataset.data.length);
+            // Check if the stock is valid
+            if(!jsonData.dataset.data) {
+              console.log('Cannot find data for the stock "' + companyList[i].name + '"');
+              return;
+            }
 
-              // Add a name and data list to each of the stocks
-              seriesOptions[i] = {
-                  name: company,
-                  data: dataList
-              };
 
-              // Keep a counter and create the chart when all the data is loaded.
-              seriesCounter += 1;
+            // Create an empty data list to be populated with stocks
+            var dataList = [ ];
+            
+            // Process jso in the following list form [[date,price],...]
+            for(var row in jsonData.dataset.data) {
+              var tempList = [ ];
+              tempList.push(new Date(jsonData.dataset.data[row][0]).getTime());
+              tempList.push(jsonData.dataset.data[row][11]);
+              dataList.push(tempList);
+            }
 
-              if (seriesCounter === companyList.length) {
-                  createChart(seriesOptions);
-              }
-          });
-      });
-  }
+            // Add a name and data list to each of the stocks
+            seriesOptions[i] = {
+                name: company.name,
+                data: dataList
+            };
 
-  /**
-   * Create the chart when all data is loaded
-   */
-  function createChart(seriesOptions) {
+            // Keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
 
-      $('#chart').highcharts('StockChart', {
+            if (seriesCounter === companyList.length) {
+                createChart(seriesOptions);
+            }
+        });
+    });
+}
 
-          rangeSelector: {
-              selected: false
-          },
+/**
+ * Create the chart when all data is loaded
+ */
+function createChart(seriesOptions) {
 
-          yAxis: {
-              labels: {
-                  formatter: function () {
-                      return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                  }
-              },
-              plotLines: [{
-                  value: 0,
-                  width: 2,
-                  color: 'silver'
-              }]
-          },
+    $('#chart').highcharts('StockChart', {
 
-          plotOptions: {
-              series: {
-                  compare: 'percent'
-              }
-          },
+        rangeSelector: {
+            selected: false
+        },
 
-          tooltip: {
-              pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-              valueDecimals: 2
-          },
+        yAxis: {
+            labels: {
+                formatter: function () {
+                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                }
+            },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+            }]
+        },
 
-          series: seriesOptions
-      });
-  }
+        plotOptions: {
+            series: {
+                compare: 'percent'
+            }
+        },
+
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2
+        },
+
+        series: seriesOptions
+    });
+}
