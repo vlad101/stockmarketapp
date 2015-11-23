19e1cc09b8f7be38aca96d4f8fadb13a05556dd7
vlad101 <V_efros85@hotmail.com>
2015-11-23 00:07:05 -0500

    completed stock market app - cleaned up and refactored code, added validation - user won't be able to add duplicate or invalid stocks

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,44 +8,89 @@ angular.module('workspaceApp')
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
       
-
       // Reload Market Chart
-      $http.get('/api/things').success(function(stockList) {
-        reloadChart(stockList);
-      });
+      $scope.refreshChart();
     });
 
     // Add a stock to a list
     $scope.addThing = function() {
 
+      // Initialize error message
+      $scope.invalidStock = '';
+
+      // Check the input stock is valid
       if($scope.newThing === '') {
+        $scope.invalidStock = "Stock is invalid, try again!";
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
 
-      // Set form to empty
-      $scope.newThing = '';
-
-      // Reload Market Chart
-      $http.get('/api/things').success(function(stockList) {
-        reloadChart(stockList);
-      });
+      // Validate stock before adding to the chart
+      $scope.addStock();
     };
+
+    // Validate stock before adding to the chart
+    $scope.addStock = function() {
+      jQuery.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/' +
+                        $scope.newThing +'.json?api_key=mytMZB31ArgfkNYgsNyn&' + 
+                        'start_date=' + getDate() + '&exclude_column_names=true', 
+            function(jsonData){
+
+                for(var company in $scope.awesomeThings) {
+                  if($scope.awesomeThings[company].name == $scope.newThing) {
+                    $scope.invalidStock = "Stock is already added, try again!";
+                    return;
+                  }
+                }
+
+                // Insert a new stock to DB
+                $http.post('/api/things', { name: $scope.newThing });
+
+                // Set form to empty
+                $scope.newThing = '';
+
+                // Reload Market Chart
+                $scope.refreshChart();
+          })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        $scope.invalidStock = "Stock is invalid, try again!";
+      });
+    }
 
     // Remove a stock to a list
     $scope.deleteThing = function(thing) {
+
+      // Delete a new stock from DB
       $http.delete('/api/things/' + thing._id);
 
       // Reload Market Chart
-      $http.get('/api/things').success(function(stockList) {
-        reloadChart(stockList);
-      });
+      $scope.refreshChart();
     };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
     });
-}); 
+
+    // Get company data and refresh chart
+    $scope.refreshChart = function() {
+      $http.get('/api/things').success(function(stockList) {
+          $scope.chartDataEmpty = false;
+          if(stockList.length == 0)
+            $scope.chartDataEmpty = true;
+          reloadChart(stockList);
+      });
+    }
+});
+
+/**
+* Get today's date
+*/
+function getDate() {
+  var date = new Date();
+  var yyyy = date.getFullYear().toString();                                    
+  var mm = (date.getMonth()+1).toString();       
+  var dd  = date.getDate().toString();             
+  return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+}
 
 /**
 * Reload chart
@@ -54,63 +99,55 @@ function reloadChart(companyList) {
 
     // Initialize chart data
     var seriesOptions = [],
-        seriesCounter = 0,
-        startDate = '2015-01-01',
-        apiKey = 'mytMZB31ArgfkNYgsNyn';
+      seriesCounter = 0,
+      startDate = '2015-01-01',
+      apiKey = 'mytMZB31ArgfkNYgsNyn';
 
 
         //CHECK THIS OUT ERROR HANDLING!
-
-        // jQuery.getJSON(
-
-        //  'https://www.quandl.com/api/v3/datasets/WIKI/' +
-        //                   'MSFT1' +'.json?api_key=' + apiKey + '&' + 
-        //                   'start_date=' + startDate + '&exclude_column_names=true&order=asc', 
-        //     function(jsonResult){
-        //         console.log("Success! "  + JSON.stringify(jsonResult));
-        //     })
-        // .done(function() { console.log('getJSON request succeeded!'); })
-        // .fail(function(jqXHR, textStatus, errorThrown) { console.log('getJSON request failed! ' + textStatus); })
-        //}
-
     $.each(companyList, function (i, company) {
-        $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/' +
-                   companyList[i].name +'.json?api_key=' + apiKey + '&' + 
-                   'start_date=' + startDate + '&exclude_column_names=true&order=asc' , function (jsonData) {
-            
-            console.log(companyList[i].name + '  -->-' +jsonData.dataset.data.length);
-            // Check if the stock is valid
-            if(!jsonData.dataset.data) {
-              console.log('Cannot find data for the stock "' + companyList[i].name + '"');
-              return;
-            }
+        jQuery.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/' +
+                          companyList[i].name +'.json?api_key=' + apiKey + '&' + 
+                          'start_date=' + startDate + '&exclude_column_names=true&order=asc', 
+                function(jsonData){
+                  var dataList = processJsonData(jsonData);
 
+                  // Add a name and data list to each of the stocks
+                  seriesOptions[i] = {
+                      name: company.name,
+                      data: dataList
+                  };
 
-            // Create an empty data list to be populated with stocks
-            var dataList = [ ];
-            
-            // Process jso in the following list form [[date,price],...]
-            for(var row in jsonData.dataset.data) {
-              var tempList = [ ];
-              tempList.push(new Date(jsonData.dataset.data[row][0]).getTime());
-              tempList.push(jsonData.dataset.data[row][11]);
-              dataList.push(tempList);
-            }
+                  // Keep a counter and create the chart when all the data is loaded.
+                  seriesCounter += 1;
 
-            // Add a name and data list to each of the stocks
-            seriesOptions[i] = {
-                name: company.name,
-                data: dataList
-            };
-
-            // Keep a counter and create the chart when all the data is loaded.
-            seriesCounter += 1;
-
-            if (seriesCounter === companyList.length) {
-                createChart(seriesOptions);
-            }
+                  if (seriesCounter === companyList.length) {
+                      createChart(seriesOptions);
+                  }
+            })
+        .fail(function(jqXHR, textStatus, errorThrown) { 
+          console.log('Something went wrong, try again!');
         });
     });
+}
+
+/**
+ * Process chart data.
+ */
+function processJsonData(jsonData)  {
+
+  // Create an empty data list to be populated with stocks
+  var dataList = [ ];
+  
+  // Process jso in the following list form [[date,price],...]
+  for(var row in jsonData.dataset.data) {
+    var tempList = [ ];
+    tempList.push(new Date(jsonData.dataset.data[row][0]).getTime());
+    tempList.push(jsonData.dataset.data[row][11]);
+    dataList.push(tempList);
+  }
+
+  return dataList;
 }
 
 /**
